857b5161c771ebb4726826713fda8ff8460e383d
prateeksingamsetty <prateeksingamsetty008@gmail.com>
2021-05-29 15:07:20 +0530

    Intermediate

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,1 +1,45 @@
-export const  LoginPage =()=> <></>
+import React from 'react'
+import { FormControl, FormGroup, FormHelperText, Input, InputLabel,FormLabel, Grid ,Box,  Card, Button,Link ,Typography} from '@material-ui/core';
+// import './Login.css'
+
+const handleFormSubmit = (e) => {
+    console.log("Submitted" + e);
+
+}
+
+const Login = () => {
+    return (
+        <div className="outerbox"  style={{position:'absolute', top:'30%', bottom:'30%', left:'35%', right:'35%',}}>   
+            {/* <Grid justify="center" alignContent="center" direction="column"> */}
+            {/* <Box display="flex" alignItems="center" justifyContent="center"> */}
+            <Card elevation={5} style={{padding:30}}>
+                <Typography style={{textAlign:'center',padding:10}} variant='h4'>Login</Typography>
+            <FormGroup>
+                <FormControl style={{margin:10}}>
+                    <InputLabel htmlFor="my-input">Email address</InputLabel>
+                    <Input  id="my-input" aria-describedby="my-helper-text" />
+                </FormControl>
+                <FormControl style={{margin:10}}>
+                    <InputLabel htmlFor="my-input">Password</InputLabel>
+                    <Input type="password" id="my-input" aria-describedby="my-helper-text" />
+                </FormControl>
+            </FormGroup>
+            <Typography style={{fontSize:15, color:"grey", position: "absolute", top:"80%" }}>New User? 
+                <Link>
+        Signup</Link>
+            </Typography>
+            <Button
+            style={{ float: "right", marginTop:10}}
+            color="primary"
+            variant="outlined"
+          >
+           Login
+          </Button>
+            {/* </Box> */}
+            </Card>
+            {/* </Grid> */}
+        </div>
+    )
+}
+
+export default Login
--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,1 +1,31 @@
-export const  ProfilePage =()=> <></>
+import React from 'react'
+import { Card, Typography, Grid } from '@material-ui/core'
+import { Row, Col } from 'react-bootstrap'
+
+const Profile = () => {
+    return (
+        <div>
+            <Card>
+                        <Grid container xs={12}>
+                            <Grid item xs={6}>
+                                Name
+                        </Grid>
+                            <Grid item xs={6}>
+                                Prateek
+                        </Grid>
+                    </Grid>
+                    <Grid xs={12} container>
+                        <Grid item xs={6}>
+                            Email
+                    </Grid>
+                        <Grid item xs={6}>
+                            Prateek@gmail.com
+                    </Grid>
+                    </Grid>
+
+            </Card>
+        </div>
+    )
+}
+
+export default Profile
